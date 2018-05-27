8aeb0026fe2fbf626415bcbe5b0985a308563242
Margatroid <351652632@qq.com>
2018-05-27 15:05:23 +0800

    change server

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import VueSocketio from 'vue-socket.io'
 Vue.config.productionTip = false
-Vue.use(VueSocketio, 'http://chat.yuyuko.cc')
+Vue.use(VueSocketio, 'https://chat.yuyuko.cc')
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
