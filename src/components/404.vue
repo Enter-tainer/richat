d4a71c31de85b04af7971fd66660d21a691afc61
Margatroid <351652632@qq.com>
2018-06-16 12:58:31 +0800

    add 404

---
+++ src/components/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>404 Not Found!</h1>
+</template>
+<script>
+export default {
+  name: 'notFound'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MessageArea from '@/components/MessageArea'
-
+import notFound from '@/components/404'
 Vue.use(Router)
 
 export default new Router({
-  routes: [ {
+  mode: 'history',
+  routes: [{
     path: '/',
     name: 'MessageArea',
     component: MessageArea
+  }, {
+    path: '/*',
+    name: 'notFound',
+    component: notFound
   }]
 })
